ff953b03e1336396d1936e1c98143cdb8ed4ae33
Aadilol <128275184+Aadilol@users.noreply.github.com>
2023-06-30 20:36:44 -0700

    Delete land directory

--- land/land.js
+++
@@ -1,7 +0,0 @@
-const bannerContainer = document.querySelector('.banner-container');
-const moveButton = document.querySelector('.move-button');
-
-
-moveButton.addEventListener('click', function() {
-  bannerContainer.classList.toggle('move-to-top');
-});
